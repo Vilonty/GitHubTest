ce6fe8cab059877ce2f0cf2cf955f44978af96f2
Vilonty <tsupshz@gmail.com>
2024-01-23 19:26:01 +0700

    Задание 2 вот тоже самое делаю

---
+++ division++.js
@@ -0,0 +1,8 @@
+alert("Доброе утро, господин!<з")
+alert("Это программа сложения, введитие пожалуйста 2 числа которые вы будете складжывать господин")
+let JoJo = prompt('Введите x')
+JoJo = parseFloat(JoJo)
+let JoJo1 = prompt('Введите y')
+JoJo1 = parseFloat(JoJo1)
+GG = JoJo + JoJo1
+alert('Результат сложения мой господин: '+JoJo+'+'+JoJo1+'='+GG)
